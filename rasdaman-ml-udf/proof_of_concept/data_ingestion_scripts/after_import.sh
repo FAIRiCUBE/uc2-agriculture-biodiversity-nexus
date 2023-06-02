4ed4806e9dcda789e6dccb308c37b6e381500030
Otoniel José Campos Escobar <oc_campos16@hotmail.com>
2023-06-02 13:00:03 +0200

    Delete rasdaman-ml-udf/proof_of_concept directory
    Delete the previous proof of concept folder for reordering

--- rasdaman-ml-udf/proof_of_concept/data_ingestion_scripts/after_import.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Calculates the max per band of coverage sentinel2_2018_flevopolder_10m_7x4bands_maxes and ingest them to collection sentinel2_2018_flevopolder_10m_7x4bands_maxes
-RASQL="rasql --user $RAS_USER --passwd $RAS_PASSWD"
-
-$RASQL 'insert into sentinel2_2018_flevopolder_10m_7x4bands_maxes values decode($1,"csv", "{ \"formatParameters\":{ \"domain\": \"[0:27]\",\"basetype\": \"ushort\" } }")' -f result.csv
